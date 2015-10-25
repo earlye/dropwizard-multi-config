062d377312aa0eb120efa4b63389b42c01b8f1f5
Early Ehlinger <earlye@gmail.com>
2015-10-25 10:06:19 -0500

    Added support for checking for overrides in the current user's home directory, using ^~/ on *nix, and ^~\ on windows.

--- src/main/java/com/thenewentity/utils/dropwizard/DefaultConfigurationReader.java
+++ src/main/java/com/thenewentity/utils/dropwizard/DefaultConfigurationReader.java
@@ -1,8 +1,10 @@
 package com.thenewentity.utils.dropwizard;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DefaultConfigurationReader implements ConfigurationReader {
@@ -10,7 +12,8 @@ public class DefaultConfigurationReader implements ConfigurationReader {
     @Override
     public String readConfiguration(String path) {
         try {
-            byte[] encoded = Files.readAllBytes(Paths.get(path));
+            Path filePath = Paths.get(path.replaceFirst("^~" + File.separator, System.getProperty("user.home") + File.separator));
+            byte[] encoded = Files.readAllBytes(filePath);
             return new String(encoded, StandardCharsets.UTF_8);
         } catch (IOException e) {
             return "";
